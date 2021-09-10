b0d513395e87af7563cfe04b39a85dab2345c586
Dmitry Korotkov <korotkov.dmitriy10.1@gmail.com>
2021-09-10 22:26:40 +0400

    functions add

--- public/homePage.js
+++ public/homePage.js
@@ -6,3 +6,95 @@ logOut.action = function () {
     }
   });
 };
+
+ApiConnector.current((res) => {
+  //console.log(res);
+  if (res.success) {
+    ProfileWidget.showProfile(res.data);
+  }
+});
+
+const rates = new RatesBoard();
+
+function getRates() {
+  ApiConnector.getStocks((res) => {
+    if (res.success) {
+      rates.clearTable();
+      rates.fillTable(res.data);
+    }
+  });
+}
+
+getRates();
+setInterval(getRates, 60000);
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = function (data) {
+  ApiConnector.addMoney(data, (res) => {
+    console.log(res);
+    if (res.success) {
+      ProfileWidget.showProfile(res.data);
+      this.setMessage(res.success, 'Пополнение баланса произошло успешно!');
+    } else {
+      this.setMessage(res.success, res.error);
+    }
+  });
+};
+
+moneyManager.conversionMoneyCallback = function (data) {
+  ApiConnector.convertMoney(data, (res) => {
+    if (res.success) {
+      ProfileWidget.showProfile(res.data);
+      this.setMessage(res.success, 'Конвертация произошла успешно!');
+    } else {
+      this.setMessage(res.success, res.error);
+    }
+  });
+};
+
+moneyManager.sendMoneyCallback = function (data) {
+  ApiConnector.transferMoney(data, (res) => {
+    if (res.success) {
+      ProfileWidget.showProfile(res.data);
+      this.setMessage(res.success, 'Валюта переведена успешно!');
+    } else {
+      this.setMessage(res.success, res.error);
+    }
+  });
+};
+
+const favorit = new FavoritesWidget();
+
+ApiConnector.getFavorites((res) => {
+  if (res.success) {
+    favorit.clearTable();
+    favorit.fillTable(res.data);
+    moneyManager.updateUsersList(res.data);
+  }
+});
+
+favorit.addUserCallback = function (data) {
+  ApiConnector.addUserToFavorites(data, (res) => {
+    if (res.success) {
+      this.clearTable();
+      this.fillTable(res.data);
+      moneyManager.updateUsersList(res.data);
+      this.setMessage(res.success, 'Пользователь добавлен!');
+    } else {
+      this.setMessage(res.success, res.error);
+    }
+  });
+};
+
+favorit.removeUserCallback = function (data) {
+  ApiConnector.removeUserFromFavorites(data, (res) => {
+    if (res.success) {
+      this.clearTable();
+      this.fillTable(res.data);
+      moneyManager.updateUsersList(res.data);
+      this.setMessage(res.success, 'Пользователь удален!');
+    } else {
+      this.setMessage(res.success, res.error);
+    }
+  });
+};
--- public/loginPage.js
+++ public/loginPage.js
@@ -2,12 +2,23 @@
 
 const user = new UserForm();
 user.loginFormCallback = function (data) {
-  console.log(data);
+  //console.log(data);
   ApiConnector.login(data, (res) => {
     if (res.success) {
       location.reload();
     } else {
-      console.log(res.error);
+      this.setLoginErrorMessage(res.error);
+    }
+  });
+};
+
+user.registerFormCallback = function (data) {
+  console.log(data);
+  ApiConnector.register(data, (res) => {
+    if (res.success) {
+      location.reload();
+    } else {
+      this.setMessage(res.succes, res.error);
     }
   });
 };
